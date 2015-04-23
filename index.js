8a29be007ad26f6c349267307ef505b8ae83def0
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-23 01:38:46 -0400

    refactor

--- index.js
+++ index.js
@@ -5,55 +5,40 @@ var pluck = require('arr-pluck');
 var merge = require('mixin-deep');
 
 module.exports = function license_(locals) {
-  var o = locals || {};
+  var o = typeof locals === 'object' ? locals : {};
 
-  // compatibility with template, verb and assemble.
+  // support Template, verb and assemble.
   if (this && this.app && this.context) {
+    this.app.union('reflinks', ['verb', 'template', 'assemble']);
     o = merge({}, this.options, this.context, o);
   }
 
-  var res = 'Released under the ';
-  var urls = [];
-
-  try {
-    if (o.licenses) {
-
-      if (o.linkify === true) {
-        o.licenses.forEach(function (license) {
-          if (license.type && license.url) {
-            urls.push(mdu.link(license.type, license.url));
-          }
-        });
-      }
-
-      if (urls.length > 0) {
-        res += urls.join(', ') + ' license' + (o.licenses.length <= 1 ? '' : 's');
-      } else {
-        res += pluck(o.licenses, 'type').join(', ') + ' license' + (o.licenses.length <= 1 ? '' : 's');
-      }
-
-    } else if (o.license) {
-      var license;
-
-      if (typeof o.license === 'object') {
-        if (o.linkify === true && o.license.type && o.license.url) {
-          license = mdu.link(o.license.type, o.license.url);
-        } else {
-          license = o.license.type;
-        }
-      }
-
-      if (typeof o.license === 'string') {
-        license = o.license;
-      }
-
-      res += license + (o.suffix || ' license');
-    }
-
-  } catch (err) {
-    err.origin = __filename;
-    throw new Error('helper-license can\'t find a "license" or "licenses" property: ', err);
+  if (!o.license && !o.licenses) {
+    throw new Error('helper-license expects a "license" or "licenses" property');
   }
 
+  if (typeof o.license === 'string') {
+    o.license = {type: o.license};
+  }
+
+  var licenses = Array.isArray(o.licenses) ? o.licenses : [o.license];
+  var res = 'Released under the ';
+  var len = licenses.length;
+  var urls = [];
+
+  if (o.linkify === true) {
+    licenses.forEach(function (license) {
+      if (license.type && license.url) {
+        urls.push(mdu.link(license.type, license.url));
+      }
+    });
+  }
+
+  var s = len <= 1 ? '.' : 's.';
+  if (urls.length > 0) {
+    res += urls.join(', ') + ' license' + s;
+  } else {
+    res += pluck(licenses, 'type').join(', ') + ' license' + s;
+  }
   return res;
 };

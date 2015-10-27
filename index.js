84bac4a8054ba192eab60b3e40c667db0cd4cfea
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-27 08:44:59 -0400

    refactor, simplify

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 'use strict';
 
-var mdu = require('markdown-utils');
-var pluck = require('arr-pluck');
-var merge = require('mixin-deep');
+module.exports = function(config, options) {
+  options = options || {};
 
-module.exports = function license_(locals) {
-  var o = typeof locals === 'object' ? locals : {};
-
-  // support Template, verb and assemble.
-  if (this && this.app && this.context) {
-    this.app.union('reflinks', ['verb', 'template', 'assemble']);
-    o = merge({}, this.options, this.app.cache.data, this.context, o);
+  if (!config || typeof config !== 'object') {
+    throw new TypeError('expected config to be an object');
   }
 
-  if (!o.license && !o.licenses) {
-    throw new Error('helper-license expects a "license" or "licenses" property');
+  var license = config.license;
+  if (typeof license !== 'string') {
+    throw new TypeError('expected license to be a string');
   }
 
-  if (typeof o.license === 'string') {
-    o.license = {type: o.license};
+  var prefix = options.prefix || 'Released under the ';
+  var suffix = ' license';
+  var res = prefix + license + suffix;
+
+  if (options.linkify === true) {
+    var fp = options.filepath || './LICENSE';
+    res = prefix + '[' + license + suffix + '](' + fp + ')';
   }
 
-  var licenses = Array.isArray(o.licenses) ? o.licenses : [o.license];
-  var res = 'Released under the ';
-  var len = licenses.length;
-  var urls = [];
-
-  if (o.linkify === true) {
-    licenses.forEach(function (license) {
-      if (license.type && license.url) {
-        urls.push(mdu.link(license.type, license.url));
-      }
-    });
-  }
-
-  var s = len <= 1 ? '.' : 's.';
-  if (urls.length > 0) {
-    res += urls.join(', ') + ' license' + s;
-  } else {
-    res += pluck(licenses, 'type').join(', ') + ' license' + s;
+  if (res[res.length - 1] !== '.') {
+    res += '.';
   }
   return res;
 };
--- test.js
+++ test.js
@@ -7,111 +7,23 @@
 
 'use strict';
 
-require('should');
-var handlebars = require('handlebars');
-var _ = require('lodash');
+require('mocha');
+var assert = require('assert');
+var pkg = require('./package');
 var helper = require('./');
 
 describe('license', function () {
-  var locals = require('./package');
-  var multiple = {
-    licenses: [
-      {
-        type: "CC by 3.0",
-        url: "https://github.com/helpers/helper-license/blob/master/LICENSE-CC"
-      },
-      {
-        type: "MIT",
-        url: "https://github.com/helpers/helper-license/blob/master/LICENSE"
-      }
-    ]
-  };
+  var pkg = {license: "MIT"};
 
-  describe('`license` string:', function () {
-    var locals = {license: "MIT"};
-
-    it('should return a formatted license statement:', function () {
-      helper(locals).should.eql('Released under the MIT license.');
-    });
-
-    it('should linkify a formatted license statement:', function () {
-      helper(locals).should.eql('Released under the MIT license.');
-    });
-
-    it('should work as a lodash helper:', function () {
-      _.template('<%= _license({license: license}) %>', {imports: {_license: helper}})(locals).should.eql('Released under the MIT license.');
-    });
-
-    it('should work as a handlebars helper:', function () {
-      handlebars.registerHelper('license', helper);
-      handlebars.compile('{{license this}}')(locals).should.eql('Released under the MIT license.');
-    });
-  });
-  describe('`licenses` array:', function () {
-    it('should return a formatted license statement:', function () {
-      helper(locals).should.eql('Released under the MIT license.');
-      helper(multiple).should.eql('Released under the CC by 3.0, MIT licenses.');
-    });
-
-    it('should work as a lodash helper:', function () {
-      _.template('<%= _license({license: license}) %>', {imports: {_license: helper}})(locals).should.eql('Released under the MIT license.');
-      _.template('<%= license({licenses: licenses}) %>', {imports: {license: helper}})(multiple).should.eql('Released under the CC by 3.0, MIT licenses.');
-    });
-
-    it('should work as a handlebars helper:', function () {
-      handlebars.registerHelper('license', helper);
-      handlebars.compile('{{license this}}')(locals).should.eql('Released under the MIT license.');
-      handlebars.compile('{{license this}}')(multiple).should.eql('Released under the CC by 3.0, MIT licenses.');
-    });
+  it('should return a formatted license statement:', function () {
+    assert.equal(helper(pkg), 'Released under the MIT license.');
   });
 
-  describe('when `linkify` is `true`:', function () {
-    it('should return a formatted license statement:', function () {
-      locals = _.extend({linkify: true}, locals);
-      multiple = _.extend({linkify: true}, multiple);
-      helper(locals).should.eql('Released under the [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) license.');
-      helper(multiple).should.eql('Released under the [CC by 3.0](https://github.com/helpers/helper-license/blob/master/LICENSE-CC), [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) licenses.');
-    });
-
-    it('should work as a lodash helper:', function () {
-      _.template('<%= _license({license: license, linkify: true}) %>', {imports: {_license: helper}})(locals).should.eql('Released under the [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) license.');
-      _.template('<%= license({licenses: licenses, linkify: true}) %>', {imports: {license: helper}})(multiple).should.eql('Released under the [CC by 3.0](https://github.com/helpers/helper-license/blob/master/LICENSE-CC), [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) licenses.');
-    });
-
-    it('should work as a handlebars helper:', function () {
-      locals = _.extend({linkify: true}, locals);
-      multiple = _.extend({linkify: true}, multiple);
-      handlebars.registerHelper('license', helper);
-
-      handlebars.compile('{{license this}}')(locals).should.eql('Released under the [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) license.');
-      handlebars.compile('{{license this}}')(multiple).should.eql('Released under the [CC by 3.0](https://github.com/helpers/helper-license/blob/master/LICENSE-CC), [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) licenses.');
-    });
+  it('should use the given prefix', function () {
+    assert.equal(helper(pkg, {prefix: 'Licensed under the '}), 'Licensed under the MIT license.');
   });
 
-  describe('when `license` is an object:', function () {
-    var locals = {
-      license: {
-        "type": "MIT",
-        "url": "https://github.com/helpers/helper-license/blob/master/LICENSE"
-      }
-    };
-
-    it('should return a formatted license statement:', function () {
-      helper(locals).should.eql('Released under the MIT license.');
-    });
-
-    it('should linkify a formatted license statement:', function () {
-      helper(_.extend({linkify: true}, locals)).should.eql('Released under the [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) license.');
-    });
-
-    it('should work as a lodash helper:', function () {
-      _.template('<%= _license({license: license, linkify: true}) %>', {imports: {_license: helper}})(locals).should.eql('Released under the [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) license.');
-    });
-
-    it('should work as a handlebars helper:', function () {
-      handlebars.registerHelper('license', helper);
-      handlebars.compile('{{license this}}')(_.extend({linkify: true}, locals)).should.eql('Released under the [MIT](https://github.com/helpers/helper-license/blob/master/LICENSE) license.');
-    });
+  it('should linkify a formatted license statement:', function () {
+    assert.equal(helper(pkg, {linkify: true}), 'Released under the [MIT license](./LICENSE).');
   });
 });
-
